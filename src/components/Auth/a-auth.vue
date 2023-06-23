4d3bfda27b35b375923bd354db4779a2a069d2a2
KotoFeelGood72 <kotenkolove@yandex.ru>
2023-06-23 13:54:24 +0000

    ss

---
+++ src/components/Auth/a-auth.vue
@@ -0,0 +1,19 @@
+<template>
+  <div  id="auth" class=" bg-dark">
+    auth
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/AuthPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id="form">
+      <ul class="form-tab-link">
+        <li v-for="item in tabs" :key="item.id">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import SignIN from "@/components/Auth/a-auth.vue";
+import SignUP from "@/components/Auth/a-sign.vue";
+export default {
+  components: {
+    SignIN,
+    SignUP,
+  },
+  data() {
+    return {
+      tabs: [
+        {
+          link: 'signIN',
+          name: 'Login',
+          id: 1
+        },
+        {
+          link: 'signUP',
+          name: 'Register',
+          id: 2
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
